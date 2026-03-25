ec598d2f4458573481967244efc50316c01b3fff
Yuthik Chitrakar <yuthikchitrakar@gmail.com>
2026-03-25 17:34:36 +0545

    Chapter 3 completed as well

---
+++ chapter3.py
@@ -0,0 +1,23 @@
+# userInput = input("Enter your name: ")
+# print(f"{userInput}, Good Afternoon!")
+# print("")
+
+# letter= '''Dear <|Name|>,
+# You are selected!
+# <|Date|>'''
+
+# print(letter.replace("<|Name|>","Yuthik").replace("<|Date|>","March 25, 2026"))
+# print("")
+
+
+# sentence = "My name is Yuthik  Chitrakar"
+# print(sentence)
+# print(sentence.find("  "))
+# print(sentence.replace("  "," "))
+
+
+letter = "Dear Yuthik,\n\tThis python course is nice.\nThanks!"
+
+print(letter)
+
+
---
+++ strings.py
@@ -0,0 +1,32 @@
+name = "Yuthik"
+
+print(name.upper())
+print(name.lower())
+
+ok = "  Yuthik   "
+# print(ok.strip())
+
+shin = "35"
+print(shin.zfill(8))
+
+fullname = "Yuthik Don"
+
+print(fullname.replace("Don","Chitrakar"))
+
+# print(len(name))
+# print(name.endswith("ik"))
+# print(name.count("Y"))
+# print(name[1])
+# print(name[:])
+# print(name[0:5])
+# print(name[:4])
+# print(name[4:])
+# print(name[-6:-1])
+
+# a="123456789123456789"
+
+# print(a[4:18:1])
+
+
+
+
