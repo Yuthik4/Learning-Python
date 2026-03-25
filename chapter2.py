530d61acf132455b4cd5475999150c932056f546
Yuthik Chitrakar <yuthikchitrakar@gmail.com>
2026-03-25 16:13:05 +0545

    Chapter 2 completed as well

---
+++ chapter2.py
@@ -0,0 +1,33 @@
+# a = 1
+# b=2
+# print(a+b)
+
+
+# a= 30
+# b=5
+# print("The remainder when a is divided by b is: ", a%b)
+
+# z = input("Enter a number: ")
+# x = int(input("Enter a number: "))
+
+# print(type(z))
+# print(type(x))
+
+
+# a= 34
+# b = 80
+
+# print("Is a greater than b: ", a > b)
+
+# a = 190
+# b= 99
+
+# print("The average of a and b is: ", (a+b)/2)
+
+import math 
+
+a = float(input("Enter the number of which you want the root of: "))
+
+root = math.sqrt(a)
+
+print("The root of the number you provided is: ",root)
---
+++ variables.py
@@ -0,0 +1,38 @@
+a = 2
+b=3
+c= a+b 
+print(c)
+
+# True or False Values
+print("True or False is", True or False)
+print("True or True is", True or True)
+print("False or True is", False or True)
+print("False or False is", False or False)
+print("")
+
+# True and Flase Values
+print("True and False is", True and False)
+print("True and True is", True and True)
+print("False and True is", False and True)
+print("False and False is", False and False)
+print("")
+
+# Using not values
+print(not(False))
+
+
+z = input("Enter a number: ")
+x = input("Enter second number: ")
+
+z = int(z)
+x=int(x)
+
+print("The first number is : ", z)
+print("The second number is : ", x)
+
+y= x+z
+
+print("The Sum is: ",y )
+
+
+
